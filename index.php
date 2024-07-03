f6738ada891d4b3ecb8c6983ac314cb262987292
TylerLillis <s3947709@student.rmit.edu.au>
2024-07-03 18:45:38 +1000

    - Setting up a search function under nav.inc
    - Setting up an image carousel on the index page
    
    - Added styleing to force the image size to be smaller within the image carousel
    
    - Added some new images "Job1.png" , "Job2.png" , "PlaceholderImage.png" in which names are just temporary and will be changed later on
    
    - Changed the link and script on the header.inc so that it is working off the official bootstrap websites links

--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <?php 
     include 'inc/header.inc';
 ?>
-
-<h1 class="jumbotron text-center"> ARTT Lawns and Garden Maintenance </h1>
-
+<div class="">
+    <h1 class="jumbotron text-center"> <img src="images/ARTTLogo.jpg" alt="logo" width="700px" height="100px"><br>Lawns and Garden Maintenance </h1>
+</div>
+<div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="images/Job1.jpg" class="d-block w-100" alt="Job1">
+        </div>
+        <div class="carousel-item">
+            <img src="images/Job2.jpg" class="d-block w-100" alt="Job2">
+        </div>
+        <div class="carousel-item">
+            <img src="images/ARTT.jpg" class="d-block w-100" alt="ARTT">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
 <?php 
     include 'inc/footer.inc';
 ?>
