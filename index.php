a9ac9bb855e7cb7690584631d5c8c2c5b4e67994
TylerLillis <s3947709@student.rmit.edu.au>
2024-07-06 17:58:28 +1000

    - Adding extra images for use as before and afters
    - Making 3 carousel and turning them into 3 before and afters for the home page
    
    - Removing the arrows from the carousel so that it automatically scrolls through before and after
    
    - Adding css to force the images into a certain sizing

--- index.php
+++ index.php
@@ -2,28 +2,67 @@
     include 'inc/header.inc';
 ?>
 <div class="">
-    <h1 class="jumbotron text-center"> <img src="images/ARTTLogo.jpg" alt="logo" width="700px" height="100px"><br>Lawns and Garden Maintenance </h1>
+    <h1 class="jumbotron text-center">
+        <img src="images/ARTTLogo.jpg" alt="logo" width="700px" height="100px"><br>Lawns and Garden Maintenance
+    </h1>
 </div>
-<div id="carouselExample" class="carousel slide">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="images/Job1.jpg" class="d-block w-100" alt="Job1">
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div id="carouselExample1" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/Job2.jpg" class="d-block w-100 mx-auto" alt="Before1">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Before</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/Job1.jpg" class="d-block w-100 mx-auto" alt="After1">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>After</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img src="images/Job2.jpg" class="d-block w-100" alt="Job2">
+        <div class="col-md-4">
+            <div id="carouselExample2" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/Job6.jpg" class="d-block w-100 mx-auto" alt="Before2">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Before</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/Job5.jpg" class="d-block w-100 mx-auto" alt="After2">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>After</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img src="images/ARTT.jpg" class="d-block w-100" alt="ARTT">
+        <div class="col-md-4">
+            <div id="carouselExample3" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/Job8.jpg" class="d-block w-100 mx-auto" alt="Before3">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Before</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/Job7.jpg" class="d-block w-100 mx-auto" alt="After3">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>After</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
 </div>
 <?php 
     include 'inc/footer.inc';
